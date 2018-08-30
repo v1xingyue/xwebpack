198046d7a567fae0825955917ae1577373c8b370
xingyue <xingyue@xsec.com>
2018-08-30 16:00:33 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import _ from 'lodash';
+import "bulma/css/bulma.min.css";
+import './css/index.css';
+import printMe from "./print.js";
+const jq = require("jquery");
+
+function component() {
+  var element = document.createElement('div');
+  element.innerHTML = _.join(['Hello', 'this is ' ,'webpack'], ' ');
+  element.classList.add("hello");
+  element.classList.add("item");
+  element.onclick = printMe;
+
+  return element;
+}
+
+
+jq(function(){
+    document.body.appendChild(component());
+});
---
+++ src/print.js
@@ -0,0 +1,11 @@
+const jq = require("jquery");
+
+jq(function(){
+    alert("---- ------");
+});
+
+export default function printMe() {
+    console.log('I get called from print.js!');
+    console.error('I get called from print.js!');
+    console.info('I get called from print.js!');
+}
