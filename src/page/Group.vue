d280d2e79c23375f187619655ed0d31c831747bc
root <root@localhost.localdomain>
2018-10-17 19:23:25 +0800

    add some default page

---
+++ src/page/Group.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>
+        <h1>Hello world {{name}}</h1>
+    </section>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            name:"One webpack project" 
+        } 
+    }
+}
+</script>
